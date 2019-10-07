06dd9a7c9bf9ced4f027ffe620641c5ef7b3dbbb
Breno Lamassa <breno@lamassa.com.br>
2019-10-07 11:09:34 +0300

    Bar stack plot and data xticks rotation

--- plot_db.py
+++ plot_db.py
@@ -5,12 +5,17 @@ plt.style.use('ggplot')
 
 df = pd.read_csv('/home/blamassa/apps/pomodoro/db.csv', header = None)
 df.columns = ['date','project']
+df['value'] = [1]*len(df)
+df['date'] = [pd.datetime.strptime(i, '%d/%m/%Y').date() for i in df['date']]
 
 fig, ax = plt.subplots()
-count_df = df.groupby('date').count()
-ax.bar(count_df.index, count_df['project'])
+
+dff = df.groupby(['date','project'],as_index=False).sum()
+pivot_df = dff.pivot(index='date', columns='project',values='value')
+pivot_df.loc[:,pivot_df.columns].plot.bar(stacked=True, ax = ax)
 
 plt.ylabel('Pomodoros')
 plt.xlabel('Datas')
 plt.title('Sessões Pomodoro por datas')
+plt.xticks(rotation = 0)
 plt.show()
