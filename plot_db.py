2d9e2fa4b1bfa1e1281421c391954fe81e365426
Breno Lamassa <breno@lamassa.com.br>
2019-10-11 15:56:45 +0300

    Xaxis now include empty days

--- plot_db.py
+++ plot_db.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+from matplotlib import dates
 import pandas as pd
+import datetime
 
 plt.style.use('ggplot')
 
@@ -8,11 +10,17 @@ df.columns = ['date','project']
 df['value'] = [1]*len(df)
 df['date'] = [pd.datetime.strptime(i, '%d/%m/%Y').date() for i in df['date']]
 
-fig, ax = plt.subplots(figsize = (20,10))
-
 dff = df.groupby(['date','project'],as_index=False).sum()
 pivot_df = dff.pivot(index='date', columns='project',values='value')
+
+### Plotting data
+fig, ax = plt.subplots(figsize = (20,10))
+
 pivot_df.loc[:,pivot_df.columns].plot.bar(stacked=True, ax = ax)
+# I want to plot all days until today. For this, I first need to know the number of days since the begining of the count.
+diff_days = (datetime.date.today() - pivot_df.index[0]).days
+datelist = [str(i)[:10] for i in pd.date_range(start = pivot_df.index[0], periods = diff_days)]
+plt.xticks(ticks = range(diff_days), labels = datelist)
 
 plt.ylabel('Pomodoros')
 plt.xlabel('Datas')
